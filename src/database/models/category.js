2344dfdc57f544c19646bbb03d5c33bd23467b3a
Eduardo Muchak <eduardomuchak@gmail.com>
2022-07-13 20:58:23 +0000

    feat(models): :sparkles: add category model

---
+++ src/database/models/category.js
@@ -0,0 +1,20 @@
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define("Category", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: true,
+    },
+    name: {
+      type: DataTypes.STRING
+    }
+  }, {
+    tableName: "Categories",
+    timestamps: false
+  }
+  );
+
+  return Category;
+};
+
+module.exports = Category;
