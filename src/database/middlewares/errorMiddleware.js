5f6df9ccb0717ad5512bfeae26c06c0aa50d061b
Eduardo Muchak <eduardomuchak@gmail.com>
2022-07-15 18:50:37 +0000

    refactor: :art: improve the error middleware
    -- Remove the switch/case and add a dinamic way to catch errors

--- src/database/middlewares/errorMiddleware.js
+++ src/database/middlewares/errorMiddleware.js
@@ -1,22 +1,6 @@
 const errorMiddleware = (err, _req, res, _next) => {
-  const { name, message } = err;
-  switch (name) {
-    case 'ValidationError':
-      res.status(400).json({ message });
-      break;
-    case 'NotFoundError':
-      res.status(404).json({ message });
-      break;
-    case 'ConflictError':
-      res.status(409).json({ message });
-      break;
-    case 'UnauthorizedError':
-      res.status(401).json({ message });
-      break;
-    default:
-      res.status(500).json({ message });
-      break;
-  }
+  const { message, status } = err;
+  return res.status(status).json({ message });
 };
 
 module.exports = errorMiddleware;
